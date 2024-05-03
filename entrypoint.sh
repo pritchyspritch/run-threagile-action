84fa0c327cd7670c50e9a50656c5564cd4419056
pritchyspritch <s.pritch@protonmail.com>
2024-05-03 12:16:59 +0100

    Add optional params

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 PATH=$PATH:/app
 cd /app
 mkdir -p "$GITHUB_WORKSPACE/threagile/output"
-threagile -model "$GITHUB_WORKSPACE/$1" -output "$GITHUB_WORKSPACE/threagile/output" -verbose
+threagile -model "$GITHUB_WORKSPACE/$1" -output "$GITHUB_WORKSPACE/threagile/output" $2 -verbose
